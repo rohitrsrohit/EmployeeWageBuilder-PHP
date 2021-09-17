cfae0a2781f21f69d09db1cc420fbc909bec4f03
Rohit Patro <rohitpatro6@gmail.com>
2021-09-17 22:32:29 +0530

    ADDED:Ability To Calculate Part Time Wage Of An Employee

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -9,19 +9,27 @@ class EmployeeWage
      public static function calcEmployeeWage()
      {
          $IS_FULL_TIME = 1;
+         $IS_PART_TIME = 0;
          $WAGE_PER_HOUR = 20;
          $FULL_WORK_HOUR = 8;
+         $PART_WORK_WAGE = 4;
 
          $random = rand(0,1);
 
          //Calculating employee daily wage
          if($random == $IS_FULL_TIME)
          {
-            print "Employee Is Present";
+            print "Employee Is Present For Full Time";
             $dailyWage = $WAGE_PER_HOUR * $FULL_WORK_HOUR;
             print " Daily Wage: ".$dailyWage;
          }
-         else
+         elseif($random == $IS_PART_TIME)
+         {
+             print "Employee Is Present For Part Time";
+             $dailyWage = $WAGE_PER_HOUR * $PART_WORK_WAGE;
+             print " Daily Wage :" .$dailyWage;
+         }
+         else 
          {
             print "Employee Is Absent";
          }
