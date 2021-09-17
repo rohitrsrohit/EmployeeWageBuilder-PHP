8052f2b8a8e075c3ef8577120e0ededff2e3884b
Rohit Patro <rohitpatro6@gmail.com>
2021-09-17 23:36:50 +0530

    ADDED:Ability To Calculate The Total wage Per month added

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -11,29 +11,30 @@ class EmployeeWage
          $IS_FULL_TIME = 1;
          $IS_PART_TIME = 0;
          $WAGE_PER_HOUR = 20;
-         $FULL_WORK_HOUR = 8;
-         $PART_WORK_WAGE = 4;
-
-         $random = rand(0,1);
-
+         $WORKING_DAYS = 20;
+         $empHrs = 0;
+         $dailyWage = 0;
+         $totalWage = 0;
+        
          //Calculating employee daily wage using Switch case statement
-         switch ($random)
-         {
-            case $IS_FULL_TIME:
-                print "Employee Is Present For Full Time";
-                $dailyWage = $WAGE_PER_HOUR * $FULL_WORK_HOUR;
-                print " Daily Wage: ".$dailyWage;
-                break;
-            case $IS_PART_TIME :
-                print "Employee Is Present For Part Time";
-                $dailyWage = $WAGE_PER_HOUR * $PART_WORK_WAGE;
-                print " Daily Wage :" .$dailyWage;
-                break;
-            default:
-                print "Employee Is Absent";
-                break;
+         for ($i=0; $i < $WORKING_DAYS ; $i++) 
+         { 
+            $random = rand(0,1);
+            switch ($random)
+            {
+                case $IS_FULL_TIME:
+                    $empHrs = 8;
+                    break;
+                case $IS_PART_TIME:
+                    $empHrs = 4;
+                    break;
+                default:
+                    $empHrs = 0;
+            }
+            $dailyWage = $WAGE_PER_HOUR * $empHrs;
+            $totalWage += $dailyWage;
          }
-            
+         echo "Salary Per month: " .$totalWage;  
      }
 }
 
