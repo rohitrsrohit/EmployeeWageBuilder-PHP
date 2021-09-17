8cd42303b76714d2eddc9c9359e7eff7b02bd6eb
Rohit Patro <rohitpatro6@gmail.com>
2021-09-17 21:42:02 +0530

    ADDED:Ability To Calculate Daily Wage Added

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -9,11 +9,23 @@ class EmployeeWage
      public static function calcEmployeeWage()
      {
          $IS_FULL_TIME = 1;
+         $WAGE_PER_HOUR = 20;
+         $FULL_WORK_HOUR = 8;
+
          $random = rand(0,1);
+
+         //Calculating employee daily wage
          if($random == $IS_FULL_TIME)
+         {
             print "Employee Is Present";
+            $dailyWage = $WAGE_PER_HOUR * $FULL_WORK_HOUR;
+            print " Daily Wage: ".$dailyWage;
+         }
          else
+         {
             print "Employee Is Absent";
+         }
+            
      }
 }
 
