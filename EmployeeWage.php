0389ae950fdc91b5ed05efcfcf2fe4daf3225f46
Rohit Patro <rohitpatro6@gmail.com>
2021-09-24 00:08:37 +0530

    ADDED:Ability To Add Company Added Through Classes an Added Associative Arrays

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,52 +1,50 @@
 <?php
 class EmployeeWage
 {
-    /**
-     * Declaring a static function and checking the Employee
-     * Attendance by using rand() function
-     */
+    public $IS_FULL_TIME = 1;
+    public $IS_PART_TIME = 0;
+   /**
+    *Declaring a static function and checking the Employee
+    *Attendance by using rand() function
+    */
+   public function calcEmployeeWage($companyName,$wagePerHr,$workingDays)
+   {
+    $empHrs = 0;
+    $dailyWage = 0;
+    $totalWage = 0;
+    $totalWorkingHrs = 0;
+    //calculating employee daily wage per month using switch.
+    for ($i=1; $i < $workingDays ; $i++) 
+    { 
+        $random = rand(0,1);
+        switch ($random) 
+        {
+        case $this -> IS_FULL_TIME:
+            $empHrs = 8;
+            break;
+        case $this -> IS_PART_TIME:
+            $empHrs = 4;
+            break;
+        default:
+            $empHrs = 0;
+        }
 
-     public static function calcEmployeeWage()
-     {
-         $IS_FULL_TIME = 1;
-         $IS_PART_TIME = 0;
-         $WAGE_PER_HOUR = 20;
-         $WORKING_DAYS = 20;
-         $empHrs = 0;
-         $dailyWage = 0;
-         $totalWage = 0;
-         $totalWorkingHours = 0;
-        
-         //Calculating employee daily wage using Switch case statement
-         for ($i=0; $i < $WORKING_DAYS ; $i++) 
-         { 
-            $random = rand(0,1);
-            switch ($random)
-            {
-                case $IS_FULL_TIME:
-                    $empHrs = 8;
-                    break;
-                case $IS_PART_TIME:
-                    $empHrs = 4;
-                    break;
-                default:
-                    $empHrs = 0;
-            }
+        //Calculating wage till this condition.
+        if($totalWorkingHrs >= 100 || $i == 20)
+        {
+            break;
+        }
+        $dailyWage = $wagePerHr * $empHrs;
+        $totalWage += $dailyWage;
+        $totalWorkingHrs += $empHrs;
+    }
 
-            //Calculationg Wage Till A Condition
-            if ($totalWorkingHours >= 100 || $i == 20) 
-            {
-                break;
-            }
-            $dailyWage = $WAGE_PER_HOUR * $empHrs;
-            $totalWage += $dailyWage;
-            $totalWorkingHours += $empHrs;
-         }
-         echo "Total Working Days: " .$i. " Total Working Hours: " .$totalWorkingHours. ","."Salary Per Month: " .$totalWage;  
-     }
+    //Associative array
+    $details = array("Company" => $companyName,"Total Working Days" => $i,"Total Working Hours " => $totalWorkingHrs,"Salary Per Month" => $totalWage);
+    print_r($details);
+    }
 }
-
-#Calling the function by using object.
+#calling the function by using object.
 $empWage = new EmployeeWage();
-$empWage -> calcEmployeeWage();
+$empWage -> calcEmployeeWage("Adani","20","20");
 ?>
